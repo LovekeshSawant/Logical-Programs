9faf57c4723d8360017acebc710ff1c3d3d72b33
LovekeshSawant <91584045+LovekeshSawant@users.noreply.github.com>
2023-12-02 12:21:24 +0530

    Add files via upload

---
+++ program106.java
@@ -0,0 +1,61 @@
+import Marvellous.MarvellousArray;
+import java.lang.*;
+import java.util.*;
+
+class Demo
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter size");
+
+		int size = sobj.nextInt();
+
+		MarvellousArray mobj = new MarvellousArray(size); 
+
+		mobj.Accept();
+		mobj.Display();
+	}
+}
+
+
+
+//array
+
+
+package Marvellous;
+
+import java.lang.*;
+import java.util.*;
+
+public class MarvellousArray
+{
+	public int arr[];					// Characteristics
+
+	public MarvellousArray(int size)	// Parametrised constructor
+	{
+		arr = new int[size];			// Dynamic memory allocation
+	}
+
+	public void Accept()				// Behaviour
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter the elements");
+
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr[i] = sobj.nextInt();
+		}	
+	}
+
+	public void Display()				// Behaviour
+	{
+		System.out.println("Entered elements are");
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.print(arr[i]+"\t");
+		}
+		System.out.println();	
+	}
+}
+
